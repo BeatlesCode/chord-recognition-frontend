9aa80221e4a489cc95a2d94b7a2011bdefb6f37d
Choi Seungeon <chou610@naver.com>
2018-06-29 10:46:32 +0900

    온라인 녹음 기능 구현

---
+++ assets/basic.js
@@ -0,0 +1,67 @@
+/**
+ * Created by chou6 on 2018-06-28.
+ */
+let shouldStop = false;
+let stopped = false;
+let mediaRecorder;
+let downLink;
+window.addEventListener('load', function () {
+    // Not showing vendor prefixes.
+    document.getElementById('upload').addEventListener('click',uploadFile);
+    document.getElementById('record').addEventListener('click',recordData);
+    document.getElementById('stop').addEventListener('click',function () {
+        mediaRecorder.stop();
+    });
+
+    downLink = document.getElementById('download');
+});
+var errorCallback = function(e) {
+    console.log('Reeeejected!', e);
+};
+function handleSuccess(stream) {
+    const options = {mimeType: 'video/webm;codecs=vp9'};
+    const recordedChunks = [];
+    mediaRecorder = new MediaRecorder(stream, options);
+
+    mediaRecorder.addEventListener('dataavailable', function(e) {
+        console.log('data available ' + e.data.size);
+        if (e.data.size > 0) {
+            recordedChunks.push(e.data);
+        }
+    });
+    mediaRecorder.addEventListener('stop', function() {
+        console.log(new Blob(recordedChunks));
+        downLink.href = URL.createObjectURL(new Blob(recordedChunks));
+        downLink.download = 'acetest.wav';
+    });
+    mediaRecorder.start();
+};
+
+
+function uploadFile(){
+    var file = document.getElementById('myFile');
+    var filedata = new FormData(); // FormData 인스턴스 생성
+    if (!file.value) return; // 파일이 없는 경우 빠져나오기
+
+    filedata.append('uploadfile', file.files[0]);
+
+    $.ajax({
+        url: "/",
+        type: 'POST',
+        data: filedata,
+        cache: false,
+        processData: false, // essential
+        contentType: false, // essential, application/pdf doesn't work.
+        enctype: 'multipart/form-data',
+
+        // If sucess, download file
+        success: function(data, status, xhr) {
+            console.log("success");
+        }
+    });
+
+}
+function recordData(){
+    navigator.mediaDevices.getUserMedia({ audio: true})
+        .then(handleSuccess)
+}
