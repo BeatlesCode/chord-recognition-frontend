e3a061831f6cda3064f1735dba9ee07682a6243a
Choi Seungeon <chou610@naver.com>
2018-06-29 14:12:12 +0900

    악보제작3

--- assets/musicScore.js
+++ assets/musicScore.js
@@ -6,21 +6,21 @@ window.addEventListener('load', function () {
 
 // Create an SVG renderer and attach it to the DIV element named "boo".
     var div = document.getElementById("boo");
-    console.log(div);
     var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 
 // Configure the rendering context.
-    renderer.resize(500, 500);
+    renderer.resize(1000, 2000);
     var context = renderer.getContext();
     context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
 
 // Create a stave of width 400 at position 10, 40 on the canvas.
     var stave = new VF.Stave(10, 40, 2000);
-
+    var stave2 = new VF.Stave(10, 80, 400);
 // Add a clef and time signature.
     stave.addClef("treble").addTimeSignature("4/4");
-
+    stave2.addClef("treble").addTimeSignature("4/4");
 // Connect it to the rendering context and draw!
     stave.setContext(context).draw();
+    stave2.setContext(context).draw();
 });
 
