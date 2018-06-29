6b640971e767a6c116a2d2a6d2a4f65bb7f3b103
Choi Seungeon <chou610@naver.com>
2018-06-29 17:28:56 +0900

    악보제작 알고리즘

--- assets/basic.js
+++ assets/basic.js
@@ -9,6 +9,7 @@ window.addEventListener('load', function () {
     // Not showing vendor prefixes.
     document.getElementById('upload').addEventListener('click',uploadFile);
     document.getElementById('record').addEventListener('click',recordData);
+    document.getElementById("audio_file").addEventListener('change',findBPM);
     document.getElementById('stop').addEventListener('click',function () {
         setTimeout(() => {
             // this will trigger one final 'ondataavailable' event and set recorder state to 'inactive'
@@ -72,4 +73,135 @@ function uploadFile(){
 function recordData(){
     navigator.mediaDevices.getUserMedia({ audio: true})
         .then(handleSuccess)
+}
+function findBPM(){
+    console.log('findBPM');
+    var file = this.files[0];
+    var reader = new FileReader();
+    var context = new(window.AudioContext || window.webkitAudioContext)();
+    reader.onload = function() {
+        context.decodeAudioData(reader.result, function(buffer) {
+            prepare(buffer);
+        });
+    };
+    reader.readAsArrayBuffer(file);
+}
+
+function prepare(buffer) {
+    var offlineContext = new OfflineAudioContext(1, buffer.length, buffer.sampleRate);
+    var source = offlineContext.createBufferSource();
+    source.buffer = buffer;
+    var filter = offlineContext.createBiquadFilter();
+    filter.type = "lowpass";
+    filter.frequency.value = 440;
+    source.connect(filter);
+    filter.connect(offlineContext.destination);
+    source.start(0);
+    offlineContext.startRendering();
+    offlineContext.oncomplete = function(e) {
+        process(e);
+    };
+}
+
+function process(e) {
+    var filteredBuffer = e.renderedBuffer;
+    //If you want to analyze both channels, use the other channel later
+    var data = filteredBuffer.getChannelData(0);
+    var max = arrayMax(data);
+    var min = arrayMin(data);
+    var threshold = min + (max - min) * 0.98;
+    var peaks = getPeaksAtThreshold(data, threshold);
+    var intervalCounts = countIntervalsBetweenNearbyPeaks(peaks);
+    var tempoCounts = groupNeighborsByTempo(intervalCounts);
+    tempoCounts.sort(function(a, b) {
+        return b.count - a.count;
+    });
+    if (tempoCounts.length) {
+        let output = document.getElementById('output');
+        output.innerHTML = tempoCounts[0].tempo;
+    }
+}
+
+
+function getPeaksAtThreshold(data, threshold) {
+    var peaksArray = [];
+    var length = data.length;
+    for (var i = 0; i < length;) {
+        if (data[i] > threshold) {
+            peaksArray.push(i);
+            // Skip forward ~ 1/4s to get past this peak.
+            i += 10000;
+        }
+        i++;
+    }
+    return peaksArray;
+}
+
+function countIntervalsBetweenNearbyPeaks(peaks) {
+    var intervalCounts = [];
+    peaks.forEach(function(peak, index) {
+        for (var i = 0; i < 10; i++) {
+            var interval = peaks[index + i] - peak;
+            var foundInterval = intervalCounts.some(function(intervalCount) {
+                if (intervalCount.interval === interval) return intervalCount.count++;
+            });
+            //Additional checks to avoid infinite loops in later processing
+            if (!isNaN(interval) && interval !== 0 && !foundInterval) {
+                intervalCounts.push({
+                    interval: interval,
+                    count: 1
+                });
+            }
+        }
+    });
+    return intervalCounts;
+}
+
+function groupNeighborsByTempo(intervalCounts) {
+    var tempoCounts = [];
+    intervalCounts.forEach(function(intervalCount) {
+        //Convert an interval to tempo
+        var theoreticalTempo = 60 / (intervalCount.interval / 44100);
+        theoreticalTempo = Math.round(theoreticalTempo);
+        if (theoreticalTempo === 0) {
+            return;
+        }
+        // Adjust the tempo to fit within the 90-180 BPM range
+        while (theoreticalTempo < 90) theoreticalTempo *= 2;
+        while (theoreticalTempo > 180) theoreticalTempo /= 2;
+
+        var foundTempo = tempoCounts.some(function(tempoCount) {
+            if (tempoCount.tempo === theoreticalTempo) return tempoCount.count += intervalCount.count;
+        });
+        if (!foundTempo) {
+            tempoCounts.push({
+                tempo: theoreticalTempo,
+                count: intervalCount.count
+            });
+        }
+    });
+    return tempoCounts;
+}
+
+// http://stackoverflow.com/questions/1669190/javascript-min-max-array-values
+function arrayMin(arr) {
+    var len = arr.length,
+        min = Infinity;
+    while (len--) {
+        if (arr[len] < min) {
+            min = arr[len];
+        }
+    }
+    return min;
+}
+
+function arrayMax(arr) {
+    var len = arr.length,
+        max = -Infinity;
+    while (len--) {
+        if (arr[len] > max) {
+            max = arr[len];
+        }
+    }
+    return max;
 }
--- assets/musicScore.js
+++ assets/musicScore.js
@@ -2,25 +2,68 @@
  * Created by chou6 on 2018-06-29.
  */
 window.addEventListener('load', function () {
-    VF = Vex.Flow;
 
+
+    renderMusicScore();
+});
+function renderMusicScore() {
+    let data = [{ "start" : "0.000000", "finish" : "0.175157", "code" : "N" },
+        { "start" : "0.175157", "finish" : "1.852358", "code" : "C" },
+        { "start" : "1.852358", "finish" : "3.454535", "code" : "G" },
+        { "start" : "3.454535", "finish" : "4.720022", "code" : "A:min" },
+        { "start" : "4.720022", "finish" : "5.126371", "code" : "A:min/b7" }];
+    /*
+    $.ajax({
+        type: 'GET',
+        url: '/',
+        data: { get_param: 'value' },
+        dataType: 'json',
+        success: function (data) {
+            $.each(data, function(index, element) {
+                data.append(element);
+            });
+        }
+    });
+    */
+    console.log(data);
+    data[length-1].start = name;
+    let i, starve;
+    let VF = Vex.Flow;
+    let NUM_OF_STAVE = 100;
 // Create an SVG renderer and attach it to the DIV element named "boo".
-    var div = document.getElementById("boo");
-    var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
-
+    let div = document.getElementById("boo");
+    let renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 // Configure the rendering context.
-    renderer.resize(1000, 2000);
-    var context = renderer.getContext();
-    context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+    renderer.resize(1000, 500);
+    let context = renderer.getContext();
+    context.setFont("Arial", 5, "").setBackgroundFillStyle("#eed");
+    let notes = [
+        // A quarter-note C.
+        new VF.StaveNote({clef: "treble", keys: ["c/4"], duration: "q" }),
+        // A quarter-note D.
+        new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "q" }),
+        // A quarter-note rest. Note that the key (b/4) specifies the vertical
+        // position of the rest.
+        new VF.StaveNote({clef: "treble", keys: ["b/4"], duration: "qr" }),
+        // A C-Major chord.
+        new VF.StaveNote({clef: "treble", keys: ["c/4", "e/4", "g/4"], duration: "q" })
+    ];
+    let voice = new VF.Voice({num_beats: 4,  beat_value: 4});
+    for(i=0; i<NUM_OF_STAVE; i++){
 
+    }
 // Create a stave of width 400 at position 10, 40 on the canvas.
-    var stave = new VF.Stave(10, 40, 2000);
-    var stave2 = new VF.Stave(10, 80, 400);
+    let stave = new VF.Stave(10, 50, 500);
+    let stave2 = new VF.Stave(10, 100, 500);
+
 // Add a clef and time signature.
     stave.addClef("treble").addTimeSignature("4/4");
     stave2.addClef("treble").addTimeSignature("4/4");
 // Connect it to the rendering context and draw!
     stave.setContext(context).draw();
     stave2.setContext(context).draw();
-});
+    voice.addTickables(notes);
+    let formatter = new VF.Formatter().joinVoices([voice]).format([voice], 100);
+    voice.draw(context, stave2);
+}
 
