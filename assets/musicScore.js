e8225ec4e668790cf331e2211a93280dc9df89bd
Choi Seungeon <chou610@naver.com>
2018-06-29 13:13:49 +0900

    악보제작1

---
+++ assets/musicScore.js
@@ -0,0 +1,22 @@
+/**
+ * Created by chou6 on 2018-06-29.
+ */
+VF = Vex.Flow;
+
+// Create an SVG renderer and attach it to the DIV element named "boo".
+var div = document.getElementById("boo")
+var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+
+// Configure the rendering context.
+renderer.resize(500, 500);
+var context = renderer.getContext();
+context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+// Create a stave of width 400 at position 10, 40 on the canvas.
+var stave = new VF.Stave(10, 40, 400);
+
+// Add a clef and time signature.
+stave.addClef("treble").addTimeSignature("4/4");
+
+// Connect it to the rendering context and draw!
+stave.setContext(context).draw();
